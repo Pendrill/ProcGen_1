00c874e912db89caae55b79b2254198a62275fcb
Aleksei Waddington <Aleksei Waddington>
2016-11-01 01:01:54 -0400

    Added more randomization. Fixed Bugs. Changed background color.

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -14,11 +14,15 @@ public class PathFinder : MonoBehaviour {
 	bool newPathFinder = false;
 
 	int rand_Pathmakers_Num;
+	float rand_turn_1;
+	float rand_turn_2;
 	// Use this for initialization
 	void Start () {
 		Total_Num_Of_Tiles = totalTilesZero;
 		//Total_Num_Of_Pathmakers = totalPathsZero;
-		rand_Pathmakers_Num = Random.Range (1, 2);
+		rand_Pathmakers_Num = Random.Range (1, 15);
+		rand_turn_1 = Random.Range (0.0f, 0.25f);
+		rand_turn_2 = Random.Range (0.25f, 0.50f);
 	}
 	
 	// Update is called once per frame
@@ -27,9 +31,9 @@ public class PathFinder : MonoBehaviour {
 		totalPathsZero = Total_Num_Of_Pathmakers;
 		if (counter <= 50 && Total_Num_Of_Tiles < 1000 ) {
 			float Random_Num = Random.Range (0.0f, 1.0f);
-			if (Random_Num <= 0.25f) {
+			if (Random_Num <= rand_turn_1) {
 				transform.Rotate (new Vector3 (0, 90f, 0));
-			} else if (Random_Num > 0.25f && Random_Num <= 0.5f) {
+			} else if (Random_Num > rand_turn_1 && Random_Num <= rand_turn_2) {
 				transform.Rotate (new Vector3 (0, -90f, 0));
 			} else if (Random_Num > 0.8f && Total_Num_Of_Pathmakers < rand_Pathmakers_Num){ //0.99f && Random_Num <= 1.0f) {
 				Instantiate (Pathmaker_Sphere_Prefab, transform.position, Quaternion.identity);
